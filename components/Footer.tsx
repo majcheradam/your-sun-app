7481bc7523dd2a5058fff635b6071a4811f2ffa3
Adam <Adammajcher20@gmail.com>
2022-12-19 22:56:25 +0100

    22:56

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,11 +5,10 @@ export default function Footer() {
     <footer className="wrapper">
       <p className="desc">
         Dzięki uprzejmości NASA/SDO oraz zespołów naukowych AIA, EVE i HMI.
+        <Link href="https://mleczni.pl">
+          <p>© Mleczni Ludzie</p>
+        </Link>
       </p>
-
-      <Link href="https://mleczni.pl">
-        <p>© Mleczni Ludzie</p>
-      </Link>
     </footer>
   )
 }
